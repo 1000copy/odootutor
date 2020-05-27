ddeb6f707f668cf5e5613277ea7e18a8f7f82e72
1000copy <1000copy@gmail.com>
2020-05-27 15:09:10 +0800

    miniing

--- document_management_system/models/document.py
+++ document_management_system/models/document.py
@@ -14,37 +14,5 @@ class Document(models.Model):
     
     sequence = fields.Integer(default=0)
     name = fields.Char('Name', required=True)
-    description = fields.Char('Description')
+    description = fields.Char('Description') 
     
-
-    
-    def name_get(self):
-        if self.env.context.get('display_full_name', False):
-            pass
-        else:
-            return super(Document, self).name_get()
-        def get_names(record):
-            res = []
-            while record:
-                res.append(record.name or '')
-                record = record.parent_id
-            return res
-        return [(record.id, " / ".join(reversed(get_names(record)))) for record in self]
-
-    
-    @api.returns('self', lambda value: value.id)
-    def copy(self, default=None):
-        default = dict(default or {})
-        default.update(name=_("%s (copy)") % (self.name or ''))
-        return super(Document, self).copy(default)
-
-    def action(self):
-        self.ensure_one()
-        context = self.env.context
-        action_id = context.get('module_action_id')
-        if action_id:
-            action_dict = self.env.ref(action_id).read([
-                "type", "res_model", "view_mode", "domain"
-            ])[0]
-            action_dict["name"] = self.name
-        return action_dict
